b9ec8aa99738eb15a70be67838468c1932d38873
Jone Zhang <NSObject_RC@163.com>
2016-07-20 20:25:48 +0800

    初始化代码

---
+++ RCScrollingFlow/RCScrollingFlow/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RCScrollingFlow
+//
+//  Created by RongCheng on 16/7/20.
+//  Copyright © 2016年 RongCheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"这只是个测试");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
